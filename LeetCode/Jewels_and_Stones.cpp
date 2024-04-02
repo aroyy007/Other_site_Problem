82518e64d9c40e5866a30392728f392c482d7ba3
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-04-02 22:25:15 +0600

    Jewels_and_Stones

---
+++ LeetCode/Jewels_and_Stones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int c=0;
+
+        for(int i=0;i<jewels.size();i++)
+        {
+            for(int j=0;j<stones.size();j++)
+            {
+                if(jewels[i]==stones[j])
+                {
+                    c++;
+                }
+            }
+        }
+
+        return c;
+    }
+};
