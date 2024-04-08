c6f43f1c893ba9074439546d8bfefe243d3df9db
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-04-08 14:45:50 +0600

    2469_Convert_the_Temperature

---
+++ LeetCode/2469_Convert_the_Temperature.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> ans;
+
+        double k = celsius + 273.15;
+        ans.push_back(k);
+        double f = celsius * 1.80 + 32.00;
+        ans.push_back(f);
+
+        return ans;
+    }
+};
