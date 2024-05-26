8973160c352fc523f41dbea163b8a4e9b451f918
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-05-26 12:02:02 +0600

    832_Flipping_an_Image

---
+++ LeetCode/Easy/832_Flipping_an_Image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        for (int i = 0; i < image.size(); i++) {
+
+            reverse(image[i].begin(), image[i].end());
+
+            for (int j = 0; j < image[i].size(); j++) {
+                image[i][j] = image[i][j] == 0 ? 1 : 0;
+            }
+        }
+        return image;
+    }
+};
