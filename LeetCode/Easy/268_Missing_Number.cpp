95f7968aae4b21e72d8057316b8f467c1fd73c5e
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-05-29 21:48:07 +0600

    268_Missing_Number

---
+++ LeetCode/Easy/268_Missing_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = accumulate(nums.begin(), nums.end(), 0);
+
+        return expectedSum - actualSum;
+    }
+};
