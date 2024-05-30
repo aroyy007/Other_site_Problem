73cf55172347c7a5c117e703d8c140c1141cc03d
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-05-30 20:05:41 +0600

    2733_Neither_Minimum_nor_Maximum

---
+++ LeetCode/Easy/2733_Neither_Minimum_nor_Maximum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+        if(nums.size()<=2)
+        {
+            return -1;
+        }
+        else
+        {
+            return nums[1];
+        }
+    }
+};
