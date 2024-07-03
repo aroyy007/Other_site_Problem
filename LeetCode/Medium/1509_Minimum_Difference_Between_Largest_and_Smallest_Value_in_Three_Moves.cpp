d4d4956e902a58281846af34f295db66045b7957
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-07-04 00:10:57 +0600

    1509_Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves

---
+++ LeetCode/Medium/1509_Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        if(nums.size()<=4){
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return min({nums[n-1]-nums[3],nums[n-2]-nums[2],nums[n-3]-nums[1],nums[n-4]-nums[0]});
+    }
+};
